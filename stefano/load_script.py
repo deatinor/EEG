00f669feff0db7affbda4dcb69c30d3b52ea9006
savare <savare@tsf-484-wpa-3-021.epfl.ch>
2018-03-19 18:47:39 +0100

    Added load script
    Signed-off-by: savare <savare@tsf-484-wpa-3-021.epfl.ch>

---
+++ stefano/load_script.py
@@ -0,0 +1,22 @@
+import torch
+import numpy as np
+from torch.autograd import Variable
+
+
+# Import default load script from parent folder
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+import dlc_bci
+
+def load_dataset(train=True):
+    dataset,target=dlc_bci.load('../data',train=train)
+
+    new_target=torch.ones(target.shape[0],2)
+    new_target[:,0][target==1]=-1
+    new_target[:,1][target==0]=-1
+    dataset=Variable(dataset)
+    target_dataset=Variable(new_target)
+
+    return dataset,target_dataset
